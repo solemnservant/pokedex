96c90a0bd9f7f93e508a6094e758995ae28df56d
solemnservant <solemnservant@gmail.com>
2024-11-07 09:50:28 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Main from './Components/Main';
+import './Components/style.css'
+
+const App = () => {
+  return (
+    <>
+    <Main />
+    </>
+  )
+}
+
+export default App
